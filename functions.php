00e9ddc65d401e34e3dbc3ade9006ae797cc4c3f
Said <babaiev.info@gmail.com>
2020-04-14 16:45:15 +0300

    Добавил тестовый Ajax запрос

--- functions.php
+++ functions.php
@@ -12,5 +12,15 @@ function sbg_favorites_scripts() {
     if(is_single() && is_user_logged_in()) {
         wp_enqueue_script('sbg-main-scripts', plugins_url('/js/main.js', __FILE__), array('jquery'), null, true);
         wp_enqueue_style('sbg-styles', plugins_url('/css/style.css', __FILE__), null, null);
+        // ajax
+        wp_localize_script( 'sbg-main-scripts', 'sbgFavorites', ['url' => admin_url('admin-ajax.php')] );  
     }
+}
+
+function wp_ajax_sbg_test() {
+    if( isset($_POST['test'])) {
+        print_r($_POST);
+    }
+
+    wp_die("запрос завершен");
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 jQuery(document).ready(function($) {
 
-    
+    $('.sbg-favorites-link a').click(function(e) {
+        $.ajax({
+            type: 'POST',
+            url: sbgFavorites.url,
+            data: {
+                test: 'test query',
+                action: 'sbg_test'
+            },
+            success: function(res) {
+                console.log(res);
+            },
+            error: function() {
+                console.log('Error')
+            }
+        });
 
+        e.preventDefault();
+    })
 });
