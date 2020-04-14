9f3079da91c73062cacb0cf5c01d5ab8934cc4d6
Said <babaiev.info@gmail.com>
2020-04-14 14:39:22 +0300

    Начало работы

---
+++ sbg-favorites.php
@@ -0,0 +1,7 @@
+/**
+ * Plugin Name: Adding Articles to Favorites
+ * Description: Плагин добавляет для авторизованных пользователей ссылку к статьям, 
+ * позволяющую добавить статью в список избранных статей
+ * Author:      Said Babaiev <babaiev.info@gmail.com>
+ * Version:     1.0
+ */
