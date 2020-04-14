a82c737f503fce90894621dc00def4d37f0910c6
Said <babaiev.info@gmail.com>
2020-04-14 15:22:59 +0300

    Добавил кнопку (добавить в избранное) к записям

--- sbg-favorites.php
+++ sbg-favorites.php
@@ -1,3 +1,4 @@
+<?php 
 /**
  * Plugin Name: Adding Articles to Favorites
  * Description: Плагин добавляет для авторизованных пользователей ссылку к статьям, 
@@ -5,3 +6,13 @@
  * Author:      Said Babaiev <babaiev.info@gmail.com>
  * Version:     1.0
  */
+
+add_filter('the_content', 'sbg_favorites_content');
+
+function sbg_favorites_content($content) {
+
+    if(is_single() && is_user_logged_in()) {
+        return '<p class="sbg-favorites-link"><a href="#">Добавить в Избранное</a></p>' . $content;
+    }
+    return $content;
+}
